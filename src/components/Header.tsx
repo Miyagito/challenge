e5387641d2c7689e0e71e68f987891342f5fa62c
jose <jose.zambrano@outsidecube.com>
2023-08-16 20:01:15 -0300

    adding theme and prettier styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
+import theme from '../utils/theme';
 
 type Props = {
-    headerTitle: string
+  headerTitle: string;
+};
+
+function Header({ headerTitle }: Props) {
+  return <div>{headerTitle}</div>;
 }
 
-function Header({headerTitle}: Props) {
-  return (
-    <div>{headerTitle}</div>
-  )
-}
-
-export default Header
+export default Header;
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,31 @@
-const colors = {
-    primary: '#FFC107',
-}
+import { createTheme } from '@mui/material/styles';
 
-export default colors;
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: '#f44336',
+    },
+    background: {
+      default: '#f5f5f5',
+    },
+  },
+  typography: {
+    fontFamily: '"Open Sans", sans-serif',
+    h1: {
+      fontSize: '2rem',
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: '1.5rem',
+      fontWeight: 600,
+    },
+  },
+});
+
+export default theme;
