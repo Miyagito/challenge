7ad5b432fda8cdb89a3a387268596fdd02612359
jose <jose.zambrano@outsidecube.com>
2023-08-17 16:22:02 -0300

    first version podcast page

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
+import Podcast from './pages/Podcast';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Podcast />,
   },
 ]);
