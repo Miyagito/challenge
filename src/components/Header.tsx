a836fff5415c9afa431543e7a4b153832d70e810
jose <jose.zambrano@outsidecube.com>
2023-08-18 13:40:37 -0300

    redirects in home and header ready

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Divider, Typography, Box } from '@mui/material';
 
 type Props = {
@@ -8,15 +9,17 @@ type Props = {
 function Header({ headerTitle }: Props) {
   return (
     <Box>
-      <Typography
-        variant="h1"
-        sx={{
-          color: (theme) => theme.palette.primary.main,
-        }}
-      >
-        {headerTitle}
-      </Typography>
-      <Divider />
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <Typography
+          variant="h1"
+          sx={{
+            color: (theme) => theme.palette.primary.main,
+          }}
+        >
+          {headerTitle}
+        </Typography>
+        <Divider />
+      </Link>
     </Box>
   );
 }
